b70ff191b97ef434466e3ae9879469e0af06980d
Islam Sadigov <51309582+azetez@users.noreply.github.com>
2022-11-12 17:36:52 +0500

    Add files via upload

---
+++ Swiftme_1.swift
@@ -0,0 +1,23 @@
+print("Решение задачи мое 👋")
+
+
+var kilometr: Double = 4
+var secund: Double = 300
+
+var metr = kilometr * 1000
+var minutes = secund / 60
+
+var speed: Double = metr / minutes
+
+print("Скорость равна ", "\(speed) метров в секунду")
+
+print("Решение задачи мое 👋")
+
+// расстояние
+var lengthOfPath = Double(4)
+// время
+var seconds = Int(300)
+// вычисление скорости в метрах в минуту
+var v = (lengthOfPath*1000) / (Double(seconds)/60)
+print("Решение автора", "\(v) метров в секунду")
+
